99c05ad1f23b69680500583160a75db7ad6e8844
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-03-11 01:16:16 +0530

    rest menu vite app done

---
+++ restaurant-menu-vite/src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from './views/Home.vue';
+import Checkout from './views/Checkout.vue';
+import TrackOrder from './views/TrackOrder.vue';
+TrackOrder;
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/checkout', component: Checkout },
+  { path: '/track-order', component: TrackOrder },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
